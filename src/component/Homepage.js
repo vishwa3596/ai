b7cc8fb464394cda6099a885d5614e8b2c08e6fb
vishwajit kumar singh <vishwa3556@gmail.com>
2019-09-08 15:01:34 +0530

    heading

--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -4,14 +4,17 @@ import {Link} from "react-router-dom"
 class Homepage extends Component{
     render() {
         return (
-
+    <div>
             <div className="homepage">
                 <h2>speakify</h2>
                 <br/>
                 <Link className="Link" style={{ textDecoration: 'none' }} to="/upload">upload</Link>
                 <Link className="record" style={{ textDecoration: 'none' }} to="/record">record</Link>
+            </div>
+            <div>
                 <Link className="about" style={{ textDecoration: 'none' }} to="/about">about</Link>
             </div>
+        </div>
         );
     }
 }
